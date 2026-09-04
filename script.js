21ae9ef5b80608d6369863561439af55480bce72
Jaishil <210993580+Jaishil07@users.noreply.github.com>
2026-09-04 10:59:47 +0000

    Add: new feature custom calculation logic
    The logic works for first iteration but in the resulting times it
    doesn't work.

--- script.js
+++ script.js
@@ -1,20 +1,97 @@
 const primaryDisplay = document.querySelector(".primary-display");
+const secondaryDisplay = document.querySelector(".secondary-display");
+const inputButtons = document.querySelectorAll(".input-numbers");
+
+const allClearButton = document.querySelector("#all-clear");
+allClearButton.addEventListener("click", allClear);
+
+let firstValue, secondValue;
+let firstOperator;
+let result;
+
+const operationButtons = document.querySelectorAll(".operations");
+operationButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        getOperateNumber(e.target.textContent);
+    });
+})
+
+inputButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        addTextToMainDisplay(e.target.value);
+    })
+});
+
 
 function addTextToMainDisplay(value) {
     primaryDisplay.textContent += value;
 }
 
-function allClear(){
-    primaryDisplay.textContent = "";    
+function addTextToSecondaryDisplay(value) {
+    secondaryDisplay.textContent += value;
 }
 
-const inputButtons = document.querySelectorAll(".input-numbers");
+function allClear() {
+    primaryDisplay.textContent = "";
+}
 
-inputButtons.forEach((button) =>{
-    button.addEventListener("click", (e)=>{
-        addTextToMainDisplay(e.target.value);
-    })
-});
+function allClearSecondary() {
+    secondaryDisplay.textContent = "";
+}
 
-const allClearButton = document.querySelector("#all-clear");
-allClearButton.addEventListener("click", allClear);
+function getOperateNumber(operator) {
+    if (primaryDisplay.textContent === "") return;
+
+    if (!firstValue) {
+        if(result){
+            allClearSecondary();
+            // addTextToSecondaryDisplay(result);
+        }
+        firstValue = +(primaryDisplay.textContent);
+        firstOperator = operator;
+        allClear();
+        addTextToSecondaryDisplay(`${firstValue} ${firstOperator}`);
+    } else {
+        secondValue = +(primaryDisplay.textContent);
+        triggerMath();
+        allClear();
+        addTextToMainDisplay(result);
+        addTextToSecondaryDisplay(` ${secondValue}`);
+
+        firstValue = undefined;
+        secondValue = undefined;
+    }
+}
+
+function triggerMath() {
+    if (firstOperator === "+") {
+        add(firstValue, secondValue);
+    }
+    else if (firstOperator === "-") {
+        subtract(firstValue, secondValue);
+    }
+    else if (firstOperator === "*") {
+        multiply(firstValue, secondValue);
+    }
+    else if (firstOperator === "/") {
+        divide(firstValue, secondValue);
+    }
+}
+
+function add(a, b) {
+    result = a + b;
+}
+
+function subtract(a, b) {
+    result = a - b;
+}
+
+function multiply(a, b) {
+    result = a * b;
+}
+
+function divide(a, b) {
+    b !== 0 ? result = a / b : result = "You cannot divide by zero...";
+}
+
+
