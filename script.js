1551dee1b8cfb0ac7da7f327f5a5f9e3564cebf3
Jaishil <210993580+Jaishil07@users.noreply.github.com>
2026-09-04 12:33:55 +0000

    Add keyboard support

--- script.js
+++ script.js
@@ -2,15 +2,43 @@ const primaryDisplay = document.querySelector(".primary-display");
 const secondaryDisplay = document.querySelector(".secondary-display");
 const inputButtons = document.querySelectorAll(".input-numbers");
 
-const allClearButton = document.querySelector("#all-clear");
-allClearButton.addEventListener("click", (e)=>{
-    allClear();
-    allClearSecondary();
-});
-
 let firstValue, secondValue;
 let firstOperator;
 let result;
+
+const dotButton = document.querySelector("#dot");
+dotButton.addEventListener("click", ()=>{
+    if(primaryDisplay.textContent.includes(".")){
+        dotButton.disabled = true;
+        return;
+    }
+    addTextToMainDisplay(".");
+})
+
+const allClearButton = document.querySelector("#all-clear");
+allClearButton.addEventListener("click", (e) => {
+    allClear();
+    allClearSecondary();
+    firstValue = undefined;
+    secondValue = undefined;
+    firstOperator = undefined;
+    result = undefined;
+});
+
+const equalButton = document.querySelector(".enter");
+equalButton.addEventListener("click", () => {
+    if (firstValue) {
+        secondValue = +(primaryDisplay.textContent);
+        triggerMath();
+        allClear();
+        addTextToMainDisplay(result);
+        addTextToSecondaryDisplay(` ${secondValue}`);
+
+        firstValue = undefined;
+        secondValue = undefined;
+    }
+});
+
 
 const operationButtons = document.querySelectorAll(".operations");
 operationButtons.forEach((button) => {
@@ -21,10 +49,19 @@ operationButtons.forEach((button) => {
 
 inputButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
+        if (firstOperator) {
+            equalButton.click;
+        }
         addTextToMainDisplay(e.target.value);
     })
 });
 
+const bkspace = document.querySelector("#backspace");
+bkspace.addEventListener("click", backspace);
+
+function backspace() {
+    primaryDisplay.textContent = primaryDisplay.textContent.slice(0, -1);
+}
 
 function addTextToMainDisplay(value) {
     primaryDisplay.textContent += value;
@@ -46,11 +83,10 @@ function getOperateNumber(operator) {
     if (primaryDisplay.textContent === "") return;
 
     if (!firstValue) {
-        if(result){
+        if (result) {
             allClearSecondary();
-            // addTextToSecondaryDisplay(result);
         }
-        firstValue = +(primaryDisplay.textContent);
+        firstValue = result ? result : +(primaryDisplay.textContent);
         firstOperator = operator;
         allClear();
         addTextToSecondaryDisplay(`${firstValue} ${firstOperator}`);
@@ -63,6 +99,7 @@ function getOperateNumber(operator) {
 
         firstValue = undefined;
         secondValue = undefined;
+        firstOperator = undefined;
     }
 }
 
@@ -82,19 +119,119 @@ function triggerMath() {
 }
 
 function add(a, b) {
-    result = a + b;
+    if (Math.floor(a + b) == a + b) {
+        result = a + b;
+    }
+    else {
+        result = (a + b).toFixed(2);
+    }
 }
 
 function subtract(a, b) {
-    result = a - b;
+    if (Math.floor(a - b) == a - b) {
+        result = a - b;
+    }
+    else {
+        result = (a - b).toFixed(2);
+    }
 }
 
 function multiply(a, b) {
-    result = a * b;
+    if (Math.floor(a * b) == a * b) {
+        result = a * b;
+    }
+    else {
+        result = (a * b).toFixed(2);
+    }
 }
 
 function divide(a, b) {
-    b !== 0 ? result = a / b : result = "You cannot divide by zero...";
+    b !== 0 ? result = (a / b).toFixed(2) : result = "Math Error";
 }
 
 
+
+// KeyBoard Accessibility
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "1") {
+        const key = document.querySelector("#one");
+        key.click();
+    } else if (e.key === "2") {
+        const key = document.querySelector("#two");
+        key.click();
+    }
+    else if (e.key === "3") {
+        const key = document.querySelector("#three");
+        key.click();
+    }
+    else if (e.key === "4") {
+        const key = document.querySelector("#four");
+        key.click();
+    }
+    else if (e.key === "5") {
+        const key = document.querySelector("#five");
+        key.click();
+    }
+    else if (e.key === "6") {
+        const key = document.querySelector("#six");
+        key.click();
+    }
+    else if (e.key === "7") {
+        const key = document.querySelector("#seven");
+        key.click();
+    }
+    else if (e.key === "8") {
+        const key = document.querySelector("#eight");
+        key.click();
+    }
+    else if (e.key === "9") {
+        const key = document.querySelector("#nine");
+        key.click();
+    }
+    else if (e.key === "0") {
+        const key = document.querySelector("#zero");
+        key.click();
+    }
+    else if (e.key === "Escape") {
+        e.preventDefault();
+        const key = document.querySelector("#all-clear");
+        key.click();
+    }
+    else if (e.key === "Enter" || e.key === "=") {
+        e.preventDefault();
+        const key = document.querySelector("#equal");
+        key.click();
+    }
+    else if (e.key === "Backspace") {
+        e.preventDefault();
+        const key = document.querySelector("#backspace");
+        key.click();
+    }
+    else if (e.key === ".") {
+        e.preventDefault();
+        const key = document.querySelector("#dot");
+        key.click();
+    }
+    else if (e.key === "+") {
+        e.preventDefault();
+        const key = document.querySelector("#plus");
+        key.click();
+    }
+    else if (e.key === "-") {
+        e.preventDefault();
+        const key = document.querySelector("#minus");
+        key.click();
+    }
+    else if (e.key === "*") {
+        e.preventDefault();
+        const key = document.querySelector("#multiply");
+        key.click();
+    }
+    else if (e.key === "/") {
+        e.preventDefault();
+        const key = document.querySelector("#divide");
+        key.click();
+    }
+
+});
